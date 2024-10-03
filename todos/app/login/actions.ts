8e6e58f98881734549503ccac45c7ea3b80ced8c
Yeukai Mutemachimwe <ymutemachimwe@gmail.com>
2024-10-03 11:19:42 +0200

    first commit- commiting all files to the repository

---
+++ todos/app/login/actions.ts
@@ -0,0 +1,59 @@
+  'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+import { Provider } from '@supabase/supabase-js'
+
+export async function login(formData: FormData) {
+  const supabase = createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/todos')
+}
+
+export async function signup(formData: FormData) {
+  const supabase = createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function oAuthSignIn(provider: Provider) {
+  if(!provider) {
+    return redirect('/login?message=No provider selected')
+  }
+  const supabase = createClient();
+  const redirectUrl = getUrl("/auth/callback")
+}
+
+function getUrl(arg0: string) {
+  throw new Error('Function not implemented.')
+}
