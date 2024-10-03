247d71258413ff28fa2ce42f347c90a2300bbc5a
Yeukai Mutemachimwe <ymutemachimwe@gmail.com>
2024-10-03 14:01:31 +0200

    Login update

--- todos/app/login/page.tsx
+++ todos/app/login/page.tsx
@@ -7,11 +7,14 @@ export default function LoginPage() {
       <input id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
       <input id="password" name="password" type="password" required />
+      <button formAction={login} className="w-full">Log in</button>
       <div className="mt-4 text-center text-sm">
         Don&apos;t have an account?{" "}
       </div>
-      <button formAction={login} className="w-full">Log in</button>
       <button formAction={signup}>Sign up</button>
+      <button form="login-form" className="underline">
+        sign up
+      </button>
     </form>
   )
 }
